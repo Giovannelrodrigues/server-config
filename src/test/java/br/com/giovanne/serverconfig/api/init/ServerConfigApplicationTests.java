2be93172670466e301c33c95be750327c2f5c770
Giovanne Lisboa Rodrigues <giovanne.rodrigues@contmatic.com.br>
2022-05-20 19:00:35 -0300

    servidor de configuração para outras apis.

---
+++ src/test/java/br/com/giovanne/serverconfig/api/init/ServerConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.giovanne.serverconfig.api.init;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
